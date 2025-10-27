0203aee8d76ad86610a639f9a9702fe5446aaac1
advaita25011fh-bit <advaita25011.fh@mitamilsangam.org>
2025-10-27 17:18:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Rapid Robots website loaded!");
+// Simple contact form handler (can be expanded with email service)
+const form = document.getElementById('contact-form');
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thank you for contacting Rapid Robots! We will get back to you soon.');
+  form.reset();
+});
